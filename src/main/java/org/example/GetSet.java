eaed968b37e28b96961aa2e55633e3d346861286
Bohdan Klymov <programmer.b.k.339@gmail.com>
2026-05-19 15:51:54 +0200

    added Getters and Setters

---
+++ src/main/java/org/example/GetSet.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class GetSet {
+
+    private String name;
+    private int age;
+    private String course;
+
+//    Getters
+    public String getName() {
+        return name;
+    }
+    public int  getAge() {
+        return age;
+    }
+    public String getCourse() {
+        return course;
+    }
+
+//    Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setCourse(String course) {
+        this.course = course;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Main {
     public static void main() {
+        GetSet student =  new GetSet();
 
+//        Setters
+        student.setName("Bohdan");
+        student.setAge(18);
+        student.setCourse("Spring-Boot-Mastering");
+
+//        Getters (output)
+        System.out.println(student.getName());
+        System.out.println(student.getAge());
+        System.out.println(student.getCourse());
     }
 }
